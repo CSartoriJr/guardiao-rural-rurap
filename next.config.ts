45bea8c1d2013f085bdba73afc9abd277518b1a0
Claudemir Sartori Junior <claauudemir@gmail.com>
2025-06-14 22:24:33 +0000

    Change the logo to https://rurap.portal.ap.gov.br/img/Logo_RURAP.png in

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'rurap.portal.ap.gov.br', // Added new domain
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
