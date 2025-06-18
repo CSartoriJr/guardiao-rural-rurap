cc7ab7a373005f8aa8c1382bb2a23d7d2642b8ee
Claudemir Sartori Junior <claauudemir@gmail.com>
2025-06-18 23:37:45 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { AuthProvider } from '@/contexts/AuthContext';
 import { Toaster } from '@/components/ui/toaster';
-import React from 'react'; // useEffect foi movido para ServiceWorkerRegistrar
+import React from 'react';
 import ServiceWorkerRegistrar from '@/components/shared/ServiceWorkerRegistrar';
 
 export const metadata: Metadata = {
@@ -22,8 +22,8 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"></link>
-        <meta name="theme-color" content="#3a8f6d" /> {/* Cor do tema para a barra de status do navegador */}
+        <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+        <meta name="theme-color" content="#3a8f6d" />
       </head>
       <body className="font-body antialiased">
         <AuthProvider>
