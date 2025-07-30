9b127ad4a90c7385a6124e459946392deb6b9200
Claudemir Sartori Junior <claauudemir@gmail.com>
2025-07-30 21:24:33 +0000

    Error: 7 PERMISSION_DENIED: Missing or insufficient permissions.     at

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -2,5 +2,4 @@ import { config } from 'dotenv';
 config();
 
 import '@/ai/flows/generate-recommendation-from-image.ts';
-// import '@/ai/flows/get-farmers-for-technician.ts'; // No longer needed
 import '@/ai/flows/update-user-by-admin.ts'; // Add new flow
