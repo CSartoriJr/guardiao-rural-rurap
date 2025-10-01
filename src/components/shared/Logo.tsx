d4fd4f7385e2321b836928668a67727a03cbd118
Claudemir Sartori Junior <claauudemir@gmail.com>
2025-10-01 15:28:40 +0000

    Na página de login, diminua o nome RURAP para metade do tamanho

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -14,7 +14,7 @@ export function CacaBruxaLogo({ className, ...props }: CacaBruxaLogoProps) {
       )}
       {...props}
     >
-      <span className="text-3xl sm:text-[45px] font-bold text-success leading-none">
+      <span className="text-xl font-bold text-success leading-none">
         RURAP
       </span>
       <span className="text-center text-[0.625rem] text-muted-foreground uppercase mt-1 tracking-wide font-bold">
